3b870e98c40f6dc6413829e6810bcd6898581824
Niv Lusky <nivmail4@gmail.com>
2026-07-21 20:20:26 +0300

    feat(health): liveness probe scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configuration, validateEnv } from './config/configuration';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { configuration, validateEnv } from './config/configuration';
       load: [configuration],
       validate: validateEnv,
     }),
+    HealthModule,
   ],
 })
 export class AppModule {}
